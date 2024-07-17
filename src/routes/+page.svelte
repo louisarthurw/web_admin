ee4eebcf208f3602ac4013ee6c93746965328e3d
louisarthurw <c14210048@john.petra.ac.id>
2024-07-17 12:59:03 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div class="flex items-center justify-center min-h-screen bg-[#F2F3F7]">
+	<div class="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+		<div class="flex justify-center mb-6">
+			<img src="/logo.png" alt="Logo Crossnet" class="h-16" />
+		</div>
+		<form>
+			<div class="mb-2">
+				<input
+					class="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#18294E] text-[#18294E]"
+					id="username"
+					type="text"
+					placeholder="Username"
+				/>
+			</div>
+			<div class="mb-8">
+				<input
+					class="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#18294E] text-[#18294E]"
+					id="password"
+					type="password"
+					placeholder="Password"
+				/>
+			</div>
+			<div class="flex items-center justify-center">
+				<button
+					class="w-full px-3 py-2 font-bold text-white bg-[#18294E] rounded-md hover:bg-[#151E39]"
+				>
+					LOGIN
+				</button>
+			</div>
+		</form>
+	</div>
+</div>
---
+++ src/routes/page2/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-[#FFFFFF]">
+	<div class="flex justify-center mb-6">
+		<img src="/logo.png" alt="Logo Crossnet" class="h-16" />
+	</div>
+	<div class="w-full max-w-lg p-6 bg-[#F2F3F7] rounded-lg shadow-md">
+		<form>
+			<div class="mt-8 mb-2">
+				<input
+					class="w-full px-3 py-1 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#18294E] text-[#18294E]" 
+					id="username"
+					type="text"
+					placeholder="Username"
+				/>
+			</div>
+			<div class="mt-1 mb-16">
+				<input
+					class="w-full px-3 py-1 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#18294E] text-[#18294E]"
+					id="password"
+					type="password"
+					placeholder="Password"
+				/>
+			</div>
+			<div class="flex items-center justify-center">
+				<button
+					class="w-full px-3 py-1 font-bold text-white bg-[#18294E] rounded-md hover:bg-[#151E39]"
+				>
+					LOGIN
+				</button>
+			</div>
+		</form>
+	</div>
+</div>
